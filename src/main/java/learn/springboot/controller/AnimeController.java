6a6fa37464c27b6d03ad913da0cab992ca7cbd0b
Leonardo Machado <machadoleonardo.eng@gmail.com>
2023-05-02 17:41:06 -0300

    aula 03 completa

--- src/main/java/learn/springboot/controller/AnimeController.java
+++ src/main/java/learn/springboot/controller/AnimeController.java
@@ -1,17 +1,27 @@
 package learn.springboot.controller;
 
 import learn.springboot.domain.Anime;
+import learn.springboot.utils.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-@RestController // Annotation para dizer que essa classe é um controller od tipo Rest
+@RestController
 @RequestMapping("anime") //localhost:8080/anime
+@Log4j2
+@RequiredArgsConstructor // Essa annotation inicia todsas as variaveis "final"
 public class AnimeController {
+    private final DateUtil dateUtil;
     @GetMapping("list")
     public List<Anime> list() {
+
+        log.info("AnimeController/(GET)list -> " + dateUtil.formatLocalToDbDateTime(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Berserk"));
     }
 }
---
+++ src/main/java/learn/springboot/utils/DateUtil.java
@@ -0,0 +1,15 @@
+package learn.springboot.utils;
+
+
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class DateUtil {
+    public String formatLocalToDbDateTime(LocalDateTime localDateTime){
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+}
