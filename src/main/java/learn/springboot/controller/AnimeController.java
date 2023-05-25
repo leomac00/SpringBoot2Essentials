b6b80cdeea7a0e0896093c8888cd6b31fa4355cc
Leonardo Machado <machadoleonardo.eng@gmail.com>
2023-05-25 16:36:58 -0300

    aula 08 completa

--- src/main/java/learn/springboot/controller/AnimeController.java
+++ src/main/java/learn/springboot/controller/AnimeController.java
@@ -1,30 +1,45 @@
 package learn.springboot.controller;
 
+import jakarta.websocket.server.PathParam;
 import learn.springboot.domain.Anime;
 import learn.springboot.serivce.AnimeService;
 import learn.springboot.utils.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("animes") //localhost:8080/anime
+@RequestMapping("animes") //localhost:8080/animes
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
     private final DateUtil dateUtil;
     private final AnimeService animeService;
-    @GetMapping("list")
-    public List<Anime> list() {
+
+    @GetMapping
+    public ResponseEntity<List<Anime>> list() {
 
         log.info("AnimeController/(GET)list -> " + dateUtil.formatLocalToDbDateTime(LocalDateTime.now()));
 
-        return animeService.listAll();
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}") //Using Path variable -> e.g.: ./1
+    public ResponseEntity<Anime> getById(@PathVariable long id) {
+        return ResponseEntity.ok(animeService.getById(id));
+    }
+
+    @GetMapping("/") //Using request param -> e.g.: ./?name=Naruto
+    public ResponseEntity<Anime> getByName(@RequestParam(name = "name") String name){
+        return ResponseEntity.ok(animeService.getByName(name));
     }
 }
--- src/main/java/learn/springboot/serivce/AnimeService.java
+++ src/main/java/learn/springboot/serivce/AnimeService.java
@@ -2,13 +2,32 @@ package learn.springboot.serivce;
 
 import learn.springboot.domain.Anime;
 import learn.springboot.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service //Adiciona esta classe como um Bean a ser mapeado pelo Srping para poder ser inicializado como serviço em outros lugares
 public class AnimeService implements AnimeRepository {
+    private List<Anime> animes = List.of(new Anime("DBZ", 1L), new Anime("Naruto", 2L));
+    private ResponseStatusException badRequest = new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found");
     public List<Anime> listAll() {
-        return List.of(new Anime("DBZ", 1L), new Anime("Naruto", 2L));
+        return animes;
     }
+    public Anime getById(long id) {
+        // Adding getById method
+        return animes.stream()
+                .filter(a -> a.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> badRequest);
+    }
+    public Anime getByName(String name) {
+        // Adding getByName method
+        return animes.stream()
+                .filter(a -> a.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow(() -> badRequest);
+    }
+
 }
